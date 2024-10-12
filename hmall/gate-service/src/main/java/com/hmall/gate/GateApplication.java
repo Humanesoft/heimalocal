6333ffce3af66fa5a3991ccbc696e0045601b59c
epic <2997207238@qq.com>
2024-10-12 11:39:27 +0800

    网关初始版本已经可以使用了，使用前端代码可以正常访问微服务了

---
+++ hmall/gate-service/src/main/java/com/hmall/gate/GateApplication.java
@@ -0,0 +1,18 @@
+package com.hmall.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GateApplication
+ * @Description TODO
+ * @Author 86152
+ * @DATE 2024/10/12 11:11
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class,args);
+    }
+}
